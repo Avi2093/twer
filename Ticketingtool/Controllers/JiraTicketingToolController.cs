c8848c5e31ca0fd3de386a9e412e489ca1151c28
Avi2093 <92841507+Avi2093@users.noreply.github.com>
2023-02-28 17:58:03 +0530

    Update JiraTicketingToolController.cs

--- Ticketingtool/Controllers/JiraTicketingToolController.cs
+++ Ticketingtool/Controllers/JiraTicketingToolController.cs
@@ -1,87 +1,112 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using Ticketingtool.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 
 namespace Ticketingtool.Controllers
 {
-
+    // This is the controller for the Jira ticketing tool.
     public class JiraTicketingToolController : Controller
     {
         private readonly ApplicationDbContext _context;
 
         public JiraTicketingToolController(ApplicationDbContext context)
         {
+            // Set the context for the controller.
             _context = context;
         }
 
-         public IActionResult Index()
+        // This action method retrieves a list of initiatives from the database and populates the dropdowns for the index view.
+        public IActionResult Index()
         {
             try
             {
-                // get all distinct descriptions where issuetype = Initiative from the database
+                // Get all distinct descriptions where issuetype = Initiative from the database.
                 var initiatives = _context.JiraTaskDetails
                     .Where(j => j.issuetype == "Initiative")
                     .Select(j => j.description)
                     .Distinct()
                     .ToList();
 
-                // create a select list for the description dropdown
+                // Create a select list for the description dropdown.
                 var descriptionSelectList = new SelectList(initiatives);
 
-                // create a view model for the index view
+                // Create a view model for the index view.
                 var viewModel = new JiraTicketingToolViewModel
                 {
-                    DescriptionSelectList = descriptionSelectList,
-                    ProjectSelectList = new SelectList(new List<SelectListItem>())
+                    InitiativeSelectList = descriptionSelectList,
+                    ProjectSelectList = new SelectList(new List<SelectListItem>()),
+                    EpicSelectList = new SelectList(new List<SelectListItem>()) // Initialize the third dropdown with an empty list.
                 };
 
+                // Render the index view with the view model.
                 return View(viewModel);
             }
             catch (Exception ex)
             {
+                // If an exception occurs, render the error view with a message.
                 var errorViewModel = new ErrorViewModel { Message = "An error occurred while processing your request. Please try again later." };
                 return View("Error", errorViewModel);
             }
         }
+
+        // This action method retrieves a list of projects for a given initiative description and populates the project dropdown.
         public IActionResult GetProjects(string description)
         {
             try
             {
-                // get all distinct jiraProjectName values for the selected description
+                // Get all distinct jiraProjectName values for the selected description.
                 var projects = _context.JiraTaskDetails
                     .Where(j => j.description == description)
-                    .Select(j => j.jiraProjectName)
+                    .Select(j => new SelectListItem
+                    {
+                        Value = j.jiraProjectName,
+                        Text = j.jiraProjectName
+                    })
                     .Distinct()
                     .ToList();
 
-                // create a select list for the project dropdown
-                var projectSelectList = new SelectList(projects);
+                // Create a select list for the project dropdown.
+                var projectSelectList = new SelectList(projects, "Value", "Text");
 
-                // return the select list as JSON
+                // Return the select list as JSON.
                 return Json(projectSelectList);
             }
             catch (Exception ex)
             {
+                // If an exception occurs, return a bad request with a message.
                 return BadRequest("An error occurred while processing your request. Please try again later.");
             }
         }
+
+        //This action method retrieves a list of epics for a given initiative description and project and populates the epic dropdown.
         public IActionResult GetEpics(string description, string project)
         {
             try
             {
-                // get all distinct epic descriptions for the selected description and project where issuetype = Epic
+                // Get the LOB value for the selected initiative description.
+                var lob = _context.JiraTaskDetails
+                    .Where(j => j.description == description && j.issuetype == "Initiative")
+                    .Select(j => j.LOB)
+                    .FirstOrDefault();
+
+                // Get all distinct epic descriptions for the selected LOB and project where issuetype = Epic.
                 var epics = _context.JiraTaskDetails
-                    .Where(j => j.description == description && j.jiraProjectName == project && j.issuetype == "Epic")
-                    .Select(j => j.description)
+                    .Where(j => j.LOB == lob && j.jiraProjectName == project && j.issuetype == "Epic")
+                    .Select(j => new SelectListItem
+                    {
+                        Value = j.description,
+                        Text = j.description
+                    })
                     .Distinct()
                     .ToList();
 
-                // create a select list for the epic dropdown
-                var epicSelectList = new SelectList(epics);
+                // Create a select list for the epic dropdown.
+                var epicSelectList = new SelectList(epics, "Value", "Text");
 
-                // return the select list as JSON
+                // Return the select list as JSON.
                 return Json(epicSelectList);
             }
             catch (Exception ex)
@@ -89,5 +114,7 @@ namespace Ticketingtool.Controllers
                 return BadRequest("An error occurred while processing your request. Please try again later.");
             }
         }
+
+
     }
 }
